ea443717b81031c6c4be9ec7c0d7ea4236a6877f
TroninDV <tronindmitr@gmail.com>
2020-04-21 20:22:23 +0300

    DAO for database added

--- app/src/main/java/com/tronindmitr/githubsearch/RepositoryItem.kt
+++ app/src/main/java/com/tronindmitr/githubsearch/RepositoryItem.kt
@@ -1,15 +1,37 @@
 package com.tronindmitr.githubsearch
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 
+
+@Entity(tableName = "repository_table")
 data class RepositoryItem(
-    val id: String,
+
+    @PrimaryKey
+    val id: Long,
+
+    @ColumnInfo(name = "repository_name")
     val name: String,
+
+    @ColumnInfo(name = "owner")
     val owner: Owner,
+
+    @ColumnInfo(name = "repository_url")
     @Json(name = "html_url") val url: String,
+
+    @ColumnInfo(name = "repository_description")
     val description: String?,
+
+    @ColumnInfo(name = "repository_language")
     val language: String?,
-    @Json(name = "created_at") val date: String
+    @ColumnInfo(name = "repository_create_date")
+    @Json(name = "created_at") val createDate: String,
+
+    @ColumnInfo(name = "repository_update_date")
+    @Json(name = "updated_at")  val updateDate: String
+
 )
 
 data class ResponseData(
