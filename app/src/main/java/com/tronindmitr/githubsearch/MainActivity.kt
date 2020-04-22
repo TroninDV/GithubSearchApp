2990e8d373afe87b0779035c28040c1e61f02d39
TroninDV <tronindmitr@gmail.com>
2020-04-22 13:03:52 +0300

    Return button from history screen added

--- app/src/main/java/com/tronindmitr/githubsearch/MainActivity.kt
+++ app/src/main/java/com/tronindmitr/githubsearch/MainActivity.kt
@@ -3,6 +3,9 @@ package com.tronindmitr.githubsearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavAction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.tronindmitr.githubsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host)
+        return navController.navigateUp()
     }
 }
